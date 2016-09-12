c62ff622111fbaf39ddecf297792eafcd31e38ec
gesellc <christian.gesell@t-online.de>
2016-09-13 00:55:26 +0200

    List mockHeroes with *ngFor

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -11,6 +11,12 @@ class Hero {
     selector: 'my-app',
     template: '''
         <h1>{{title}}</h1>
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+          <li *ngFor="let hero of heroes">  <!-- // The (*) prefix to ngFor indicates that the <li> element and its children constitute a master template. -->
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+          </li>
+        </ul>
         <h2>{{hero.name}} details!</h2>
         <div><label>id: </label>{{hero.id}}</div>
         <div>
@@ -22,6 +28,7 @@ class AppComponent {
   String title = 'Tour of Heroes';
   //var hero = 'Windstorm';
   Hero hero = new Hero(1, 'Windstorm');
+  final List<Hero> heroes = mockHeroes;
 }
 
 final List<Hero> mockHeroes = [
@@ -39,3 +46,4 @@ final List<Hero> mockHeroes = [
 
 // TODO: Learn more about interpolation in the Displaying Data chapter. https://angular.io/docs/dart/latest/guide/displaying-data.html
 // TODO: Learn more about ngModel in the Forms and Template Syntax chapters. https://angular.io/docs/dart/latest/guide/forms.html#ngModel https://angular.io/docs/dart/latest/guide/template-syntax.html#ngModel
+// TODO: Learn more about ngFor and template input variables in the Displaying Data and Template Syntax chapters. https://angular.io/docs/dart/latest/guide/displaying-data.html#ngFor https://angular.io/docs/dart/latest/guide/template-syntax.html#ngFor
