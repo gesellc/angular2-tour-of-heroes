dacd8f0c74bf1fa6f7718bcd6943e36826ecc0c3
gesellc <christian.gesell@t-online.de>
2016-09-12 22:09:32 +0200

    Extend template

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,11 @@
 import 'package:angular2/core.dart';
 @Component(
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details</h2>')
+    template: '''
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div><label>name: </label>{{hero.name}}</div>''')
 class AppComponent {
 
   String title = 'Tour of Heroes';
