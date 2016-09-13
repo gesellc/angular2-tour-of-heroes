b328982a7fcbbb5928ed4fb14133965cff943da0
gesellc <christian.gesell@t-online.de>
2016-09-13 17:12:13 +0200

    Make hero property an input

--- lib/hero_detail_component.dart
+++ lib/hero_detail_component.dart
@@ -15,5 +15,8 @@ import 'hero.dart';
         </div>''',
     )
 class HeroDetailComponent {
+  @Input()
   Hero hero;
 }
+
+// TODO: Learn more about @Input() in the Attribute Directives chapter. https://angular.io/docs/dart/latest/guide/attribute-directives.html#input
