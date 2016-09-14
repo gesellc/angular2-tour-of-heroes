17f6d22ffa51ea05357f413ab756ead220d061e1
gesellc <christian.gesell@t-online.de>
2016-09-14 19:18:18 +0200

    Create dashboard and the navigation between dashboard and heroes list

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,18 +3,23 @@ import 'package:angular2/router.dart';
 
 import 'heroes_component.dart';
 import 'hero_service.dart';
+import 'dashboard_component.dart';
 
 @Component(
     selector: 'my-app',
     template: '''
       <h1>{{title}}</h1>
-      <a [routerLink]="['Heroes']">Heroes</a>
+      <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+      </nav>
       <router-outlet></router-outlet>
       ''',
     directives: const [ROUTER_DIRECTIVES],
     providers: const [HeroService, ROUTER_PROVIDERS])
 @RouteConfig(const [
-  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent)
+  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent),
+  const Route(path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true)
 ])
 class AppComponent {
   String title = 'Tour of Heroes';
