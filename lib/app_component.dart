460e97fe78b933bbe42a5d0c6533738ee7c4ffc4
gesellc <christian.gesell@t-online.de>
2016-09-14 19:00:14 +0200

    Working Route from AppComponent to HeroesComponent

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -8,12 +8,18 @@ import 'hero_service.dart';
     selector: 'my-app',
     template: '''
       <h1>{{title}}</h1>
-      <my-heroes></my-heroes>
+      <a [routerLink]="['Heroes']">Heroes</a>
+      <router-outlet></router-outlet>
       ''',
     directives: const [ROUTER_DIRECTIVES],
     providers: const [HeroService, ROUTER_PROVIDERS])
+@RouteConfig(const [
+  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent)
+])
 class AppComponent {
   String title = 'Tour of Heroes';
 }
 
 // TODO: The Routing and Navigation chapter covers the router in more detail than we will in this tutorial. The Routing and Navigation chapter covers the router in more detail than we will in this tutorial.
+// TODO: Learn more about defining routes with @RouteConfig in the Routing chapter. https://angular.io/docs/dart/latest/guide/router.html
+// TODO: Learn about the link parameters list in the Routing chapter. https://angular.io/docs/dart/latest/guide/router.html#link-parameters-array
