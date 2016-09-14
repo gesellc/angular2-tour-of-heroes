1369709f5ce8ab48957e97e5cc46d62e0514fbe6
gesellc <christian.gesell@t-online.de>
2016-09-14 22:37:15 +0200

    Add parameterized route to HeroDetailComponent

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,6 +4,7 @@ import 'package:angular2/router.dart';
 import 'heroes_component.dart';
 import 'hero_service.dart';
 import 'dashboard_component.dart';
+import 'hero_detail_component.dart';
 
 @Component(
     selector: 'my-app',
@@ -19,7 +20,8 @@ import 'dashboard_component.dart';
     providers: const [HeroService, ROUTER_PROVIDERS])
 @RouteConfig(const [
   const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent),
-  const Route(path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true)
+  const Route(path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true),
+  const Route(path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent),
 ])
 class AppComponent {
   String title = 'Tour of Heroes';
