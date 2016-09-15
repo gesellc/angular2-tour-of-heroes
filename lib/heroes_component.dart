618911d1e91829653f1f64ea73591c145a5d10c7
gesellc <christian.gesell@t-online.de>
2016-09-15 21:59:21 +0200

    Factor html template and css out of heroes component dart file

--- lib/heroes_component.dart
+++ lib/heroes_component.dart
@@ -7,74 +7,8 @@ import 'hero_service.dart';
 
 @Component(
     selector: 'my-heroes',
-    template: '''
-      <h2>My Heroes</h2>
-      <ul class="heroes">
-        <li *ngFor="let hero of heroes"
-          [class.selected]="hero == selectedHero"
-          (click)="onSelect(hero)">
-          <!-- // The (*) prefix to ngFor indicates that the <li> element and its children constitute a master template. -->
-          <!-- property binding, a binding in which data flows one way from the data source (the expression hero == selectedHero) to a property of class -->
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-      </ul>
-      <div *ngIf="selectedHero != null">
-          <h2>
-            {{selectedHero.name | uppercase}} is my hero
-          </h2>
-        <button (click)="gotoDetail()">View Details</button>
-      </div>
-    ''',
-    styles: const [
-      '''
-      .selected {
-        background-color: #CFD8DC !important;
-        color: white;
-      }
-      .heroes {
-        margin: 0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 10em;
-      }
-      .heroes li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0em;
-        height: 1.6em;
-        border-radius: 4px;
-      }
-      .heroes li.selected:hover {
-        color: white;
-      }
-      .heroes li:hover {
-        color: #607D8B;
-        background-color: #d3d6d8;
-        /* left: .1em; */
-      }
-      .heroes .text {
-        position: relative;
-        top: -3px;
-      }
-      .heroes .badge {
-        display: inline-block;
-        font-size: small;
-        color: white;
-        padding: 0.8em 0.7em 0em 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -4px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0px 0px 4px;
-      }
-    '''
-    ],
+    templateUrl: 'heroes_componentqqqq.html',
+    styleUrls: const ['heroes_component.css'],
     directives: const [HeroDetailComponent],
     )
 class HeroesComponent implements OnInit {
