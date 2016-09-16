62aaa3f93169ca56ecd94072d74edf4e30d4a834
gesellc <christian.gesell@t-online.de>
2016-09-16 19:11:09 +0200

    Create InMemoryDatabaseService

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,17 @@
+import 'package:angular2/core.dart';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2_quickstart/app_component.dart';
+import 'package:angular2_quickstart/in_memory_data_service.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 
 void main() {
-  bootstrap(AppComponent);
+  bootstrap(AppComponent, [
+    [provide(Client, useClass: InMemoryDataService)]
+    // Using a real back end? Import browser_client.dart and change the above to
+    // [provide(Client, useFactory: () => new BrowserClient(), deps: [])]
+  ]);
+  // Simplify bootstrap provider list to [BrowserClient]
+  // once there is a fix for:
+  // https://github.com/dart-lang/angular2/issues/37
 }
