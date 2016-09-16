196a6ab228958b63203b2d58beafd25568549364
gesellc <christian.gesell@t-online.de>
2016-09-16 23:57:14 +0200

    Remove stray character...

--- lib/in_memory_data_service.dart
+++ lib/in_memory_data_service.dart
@@ -13,7 +13,7 @@ class InMemoryDataService extends MockClient {
   static final _initialHeroes = [
     {'id': 11, 'name': 'Mr. Nice'},
     {'id': 12, 'name': 'Narco'},
-    {'id': 13, 'name':q 'Bombasto'},
+    {'id': 13, 'name': 'Bombasto'},
     {'id': 14, 'name': 'Celeritas'},
     {'id': 15, 'name': 'Magneta'},
     {'id': 16, 'name': 'RubberMan'},
